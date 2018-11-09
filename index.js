9f566e96b2b3d43b0262853557f460b81623cf35
jemikanegara <jemikanegara@gmail.com>
2018-11-09 15:17:29 +0700

    add package.json + CRUDS

--- index.js
+++ index.js
@@ -38,12 +38,77 @@ const User = sequelize.define("user", {
   }
 });
 
-User.create({
-  first_name: "Susi",
-  last_name: "Susanti",
-  email: "susi@gmail.com"
-})
-  .then(() => console.log("berhasil"))
-  .catch(err => console.log(`gagal ${err}`));
-
 // User.sync({ force: true })
+
+// ----------------------CREATE------------------------------
+
+// User.create({
+//   first_name: "Jojo",
+//   last_name: "Jajan",
+//   email: "jojojajan@gmail.com"
+// })
+//   .then(() => console.log("berhasil"))
+//   .catch(err => console.log(`gagal ${err}`));
+
+// ----------------------READ BY ID------------------------------
+
+// User.findById(1, { raw: true })
+//   .then(user => console.log(user))
+//   .catch(err => console.log(err));
+
+// -----------------------READ ALL--------------------------
+
+// User.findAll({ raw: true })
+//   .then(user => console.log(user))
+//   .catch(err => console.log(err));
+
+// ----------------------UPDATE ALL----------------------------
+
+// User.update(
+//   {
+//     first_namessasas: "wippy"
+//   },
+//   {
+//     where: {
+//       id: 1
+//     }
+//   }
+// )
+//   .then(result => {
+//     console.log(`berhasil`);
+//   })
+//   .catch(err => console.log(err));
+
+// ---------------------DELETE-------------------------
+
+// User.findOne({ where: { id: 4 } })
+//   .then(user =>
+//     user
+//       .destroy()
+//       .then(res => console.log(res))
+//       .catch(err => console.log(err))
+//   )
+//   .catch(err => console.log(err));
+
+// ---------------------DELETE ALL----------------------
+
+// User.findAll({ where: { last_name: "budi" }, raw: true })
+//   .then(user => console.log(user))
+//   .catch(err => console.log(err));
+
+// ---------------------- DELETE WITH OR & AND ----------------
+// const Op = Sequelize.Op;
+
+// User.findAll({
+
+//   where: {
+//     [Op.or]: [
+//       { last_name: "budi" },
+//       { first_name: "roni", [Op.and]: { last_name: "rono" } },
+//       { first_name: "jojo" }
+//     ]
+//   },
+//   raw: true
+// })
+//   .then(user => console.log(user))
+//   .catch(err => console.log(err));
